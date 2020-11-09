8b5959bd544ce7d8733f4489cc6cb8bb75675c99
Ricardo A Martinez <rickymartinez714@gmail.com>
2020-11-08 16:46:34 -0800

    Initial Commit

---
+++ Restaurant App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Restaurant App
+//
+//  Created by Ricky martinez on 11/8/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
